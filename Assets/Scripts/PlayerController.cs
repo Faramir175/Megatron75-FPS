20f92d5f18b6346f2cd38c19dd24d97721b22ccd
Faramir175 <faramilan1@gmail.com>
2024-07-12 22:54:32 +0200

    Trcanje i skakanje

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed, gravityModifier, jumpPower, runSpeed = 12f;
     public CharacterController charCon;
 
     private Vector3 moveInput;
@@ -14,6 +14,10 @@ public class PlayerController : MonoBehaviour
     public bool invertX;
     public bool invertY;
 
+    private bool canJump,canDoubleJump;
+    public Transform groundCheckpoint;
+    public LayerMask whatIsGround; 
+
     void Start()
     {
         
@@ -22,10 +26,52 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Kretanje igraca
-        moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
-        charCon.Move(moveInput);
+        //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        //za gravitaciju
+        float yStore = moveInput.y;
+
+        Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horiMove = transform.right * Input.GetAxis("Horizontal");
+
+        moveInput = horiMove + vertMove;
+        moveInput.Normalize();
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveInput = moveInput*runSpeed;
+        }
+        else { 
+            moveInput = moveInput * moveSpeed;
+        }
+
+        moveInput.y = yStore;
+        moveInput.y += Physics.gravity.y * gravityModifier*Time.deltaTime;
+
+        if (charCon.isGrounded)
+        {
+            moveInput.y = Physics.gravity.y*gravityModifier*Time.deltaTime ;
+        }
+
+        //Dodavanje skoka
+        canJump = Physics.OverlapSphere(groundCheckpoint.position, .25f, whatIsGround).Length > 0;
+        if(canJump)
+        {
+            canDoubleJump = false;
+        }
+
+        if (canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            moveInput.y = jumpPower;
+            canDoubleJump = true;
+        }else if(canDoubleJump && Input.GetKeyDown(KeyCode.Space)) 
+        { 
+            moveInput.y = jumpPower; canDoubleJump = false; 
+        }
+
+        charCon.Move(moveInput * Time.deltaTime);
 
         //Kontrolisanje rotacije kamere
 
